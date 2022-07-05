0f3858a27dddc2402f220bb5fc4763e969991931
taku <integral0523@gmail.com>
2022-07-05 17:24:39 +0900

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,10 +40,10 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  // axios: {
+  //   // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+  //   baseURL: '/',
+  // },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -75,7 +75,7 @@ export default {
   build: {},
 
   router: {
-    base: '/my-nuxt-project/',
+    base: '/addressInput/',
   },
 
   generate: {
